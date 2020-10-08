bb016b7a7ff5cd9b87c6765cfefeb33a6b48e698
Dave-lap <kissiofbodom@gmail.com>
2020-10-08 12:51:08 -0500

    integracion de login con facebook p2 completa

--- app/src/main/java/com/example/auth_test/HomeActivity.kt
+++ app/src/main/java/com/example/auth_test/HomeActivity.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.facebook.login.LoginManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
 import kotlinx.android.synthetic.main.activity_home.*
 
 enum class ProviderType{
@@ -15,6 +19,8 @@ enum class ProviderType{
     FACEBOOK
 }
 class HomeActivity : AppCompatActivity() {
+
+    private  val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -33,6 +39,18 @@ class HomeActivity : AppCompatActivity() {
         prefs.putString("provider",provider)
         prefs.apply()
 
+        //Remote Config
+        btnError.visibility = View.INVISIBLE
+        Firebase.remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                val showErrorButton = Firebase.remoteConfig.getBoolean("show_error_button")
+                val errorButtonText = Firebase.remoteConfig.getString("error_button_text")
+                if(showErrorButton){
+                    btnError.visibility = View.VISIBLE
+                }
+                btnError.text = errorButtonText
+            }
+        }
 
     }
 
@@ -58,7 +76,26 @@ class HomeActivity : AppCompatActivity() {
             FirebaseCrashlytics.getInstance().setUserId(email)
             FirebaseCrashlytics.getInstance().setCustomKey("Provider", provider)
             //Forzado de error
-            throw RuntimeException("Forzado de error")
+           // throw RuntimeException("Forzado de error")
+        }
+
+        btnSave.setOnClickListener {
+
+            db.collection("users").document(email).set(
+                hashMapOf("provider " to provider,
+                "address" to tvAddress.text.toString(),
+                "phone" to tvPhone.text.toString())
+            )
+        }
+        btnGet.setOnClickListener {
+            db.collection("users").document(email).get().addOnSuccessListener {
+                tvAddress.setText(it.get("address") as String?)
+                tvPhone.setText(it.get("phone") as String?)
+            }
+        }
+        btnDelete.setOnClickListener {
+            db.collection("users").document(email).delete()
+
         }
 
     }
